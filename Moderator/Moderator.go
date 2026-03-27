b92aadb8e48316c3914c4370eaa378ae7e739bd7
Piotr Szerłomski <pszerlomski@gmail.com>
2026-03-27 16:30:51 +0100

    add file opener to go moderator

--- Moderator/Moderator.go
+++ Moderator/Moderator.go
@@ -1,7 +1,41 @@
 package main
 
-var banedWords = []string{"badword1", "badword2", "badword3"}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var badWords []string
+
+func loadBadWords() ([]string, error) {
+	var words []string
+	file, err := os.Open("Moderator/badwords.csv")
+	if err != nil {
+		fmt.Printf("Nie udało się otworzyś pliku: %v\n", err)
+		return nil, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" {
+			words = append(words, strings.Split(line, ";")...)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Błąd podczas odczytywania pliku: %v\n", err)
+		return nil, err
+	}
+	return words, nil
+}
 
 func main() {
-
+	var err error
+	badWords, err = loadBadWords()
+	if err != nil {
+		fmt.Printf("Błąd podczas ładowania słów niedozwolonych: %v\n", err)
+		return
+	}
 }
